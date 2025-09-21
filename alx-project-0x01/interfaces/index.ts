c016fcd66d2d170b7d13c57fa574e2bac3470495
akrate <oussamaakrate590@gmail.com>
2025-09-21 15:55:55 +0100

    test

---
+++ alx-project-0x01/interfaces/index.ts
@@ -0,0 +1,73 @@
+import { StringLiteral } from "typescript";
+
+export interface PostProps {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+export interface UserProps {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: number;
+        geo: {
+            lat: number;
+            lng: number;
+        }
+    };
+    phone: number;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    }
+}
+
+export interface PostData {
+    userId: number;
+    id?: number;
+    title: string;
+    body: string;
+}
+
+export  interface PostModalProps{ 
+    onClose: () => void;
+    onSubmit: (post: PostData) => void;
+
+}
+
+export interface UserData {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: number;
+        geo: {
+            lat: number;
+            lng: number;
+        }
+    };
+    phone: number;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    }
+}
+
+export interface UserModalProps{
+    onSubmit: (post: UserProps) => void;
+}
